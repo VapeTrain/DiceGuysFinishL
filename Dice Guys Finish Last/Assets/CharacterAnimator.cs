d4fb50b7bedd283621f75436a227456546f918a9
VapeTrain <fugatt97@gmail.com>
2019-04-07 23:10:13 -0700

    Coded some of the animations for the character to actually look like he's walking around, still working out the kinks ~Bryce

---
+++ Dice Guys Finish Last/Assets/CharacterAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public Animator Locomotion;
+    public float xMov;
+    public float yMov;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Locomotion = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMov = Input.GetAxisRaw("Horizontal");
+        Locomotion.SetFloat("xMov", xMov);
+        yMov = Input.GetAxisRaw("Vertical");
+        Locomotion.SetFloat("yMov", yMov);
+    }
+}
